7212e50efd4f2e28308fb77ce14ebe0fb8ee8d89
mfbetancourtp <42900005+mfbetancourtp@users.noreply.github.com>
2022-09-03 09:53:19 -0500

    Create producto.sql

---
+++ producto.sql
@@ -0,0 +1,17 @@
+create table producto(
+    id_producto int primary key,
+    nombre_fabricante char(100),
+    precio INT
+);
+
+INSERT INTO producto VALUES (1, "Cannondale", 1200000);
+INSERT INTO producto VALUES (2, "Trek", 1450000);
+INSERT INTO producto VALUES (3, "Yeti", 2000000);
+INSERT INTO producto VALUES (4, "Fuji", 950000);
+INSERT INTO producto VALUES (5, "BMC", 1950000);
+INSERT INTO producto VALUES (6, "Starker", 4200000);
+INSERT INTO producto VALUES (7, "Lucky Lion", 5600000);
+INSERT INTO producto VALUES (8, "Be Electric", 4600000);
+INSERT INTO producto VALUES (9, "Aima", 8000000);
+INSERT INTO producto VALUES (10, "Mec de Colombia", 5900000);
+INSERT INTO producto VALUES (11, "Atom Electric", 4500000);
